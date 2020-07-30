e1009fce39d7764f9aa4056b1b5fee5561c7c46f
Winnie Yeung <vionwinnie@gmail.com>
2020-07-29 20:44:31 -0700

    gcp app engine config

--- manage.py
+++ manage.py
@@ -7,6 +7,13 @@ import sys
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newsApp.settings')
     try:
+        import googleclouddebugger
+        googleclouddebugger.enable(
+            breakpoint_enable_canary=True
+        )
+    except ImportError:
+        pass
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
