9bc3413969e0f70ada31ab04a8135af25071b529
mit <mit945.mc@gmail.com>
2020-08-19 17:53:22 -0700

    updating viz to dev and migratedstill no images

--- classifier/models.py
+++ classifier/models.py
@@ -21,8 +21,8 @@ class Category(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE)
     category = models.CharField(max_length=100)
     prob_score = models.FloatField(default=0)
-    # viz = models.TextField()
+    viz = models.TextField(max_length=500, blank=True)
 
-    
+
     def __str__(self):
         return self.category
